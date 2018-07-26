ff30a239d97669658ca35864c0aea13ab90f6afa
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-26 01:04:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,22 +12,26 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
   var index =0
 
- document.addEventListener("keypress",(event)=>{
+  document.addEventListener("keypress",(event)=>{
+   
    var keyName = event.key
+   
    if(keyName===codes[index]){
      index++
+     
      if(index===codes.length-1){
        alert("Codes Entered Correctly");
        index=0
+       
      }
+     
    }else{
+     
      index=0
+     
    }
  })
-  
-  
-  
 }
