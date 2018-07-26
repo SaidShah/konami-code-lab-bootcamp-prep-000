657ded5ef4e47d77d97dcae33e1667078a644d86
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-26 01:00:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,21 @@ const codes = [
 
 function init() {
   // your code here
+  var index =0
+  var keysPressed =[]
+ document.addEventListener("keypress",(event)=>{
+   var keyName = event.keypress
+   if(keyName===codes[index]){
+     index++
+     if(index===codes.length){
+       alert("Codes Entered Correctly")
+       index=0
+     }
+   }else{
+     index=0
+   }
+ })
+  
+  
+  
 }
